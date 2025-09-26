1c75733b34725a016f44f778836f7170d4061b77
AgriDulce7u7 <142194231+AgriDulce7u7@users.noreply.github.com>
2025-09-25 21:35:12 -0500

    v1

---
+++ src/main/java/org/example/Aplicacion.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        ListaSimple lista = new ListaSimple();
+
+        lista.agregarInicio(1);
+        lista.agregarFinal(2);
+        lista.agregarInicio(3);
+        lista.eliminar(1);
+
+        System.out.println(lista.print());
+    }
+}
---
+++ src/main/java/org/example/ListaSimpleIterador.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Iterator;
+
+public class ListaSimpleIterador implements Iterator<Integer> {
+
+    Nodo aux;
+
+    public ListaSimpleIterador(Nodo primero){
+        aux = primero;
+    }
+    @Override
+    public boolean hasNext() {
+        return aux.getSiguiente() != null;
+    }
+
+    @Override
+    public Integer next() {
+        int valor = aux.getValor();
+        aux = aux.getSiguiente();
+        return valor;
+    }
+}
